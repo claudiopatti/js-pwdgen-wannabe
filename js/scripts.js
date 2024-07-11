280b046d75252def975b712091484377f9d785ad
claudiopatti <claudiopatti1997@gmail.com>
2024-07-11 09:16:10 +0200

    esercizio aggiunte le caratteriste richieste dall'esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@
 const name = prompt("Qual'è il tuo nome ?");
 console.log ('Nome: ', name )
 
+
+const surname = prompt("Qual'è il tuo cognome ?");
+console.log ('cognome: ', surname )
+
+
+const color = prompt("Qual'è il tuo colore preferito ?");
+console.log ('colore: ', color )
+
+console.log (name + surname + color + '130')
+
+
+document.getElementById('Password').innerHTML =  name + surname + color + '130';
+
